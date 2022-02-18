3428a213fe74c8702e74a88bd34496f8d73d8b31
MateusJSouza <mateusjdev@gmail.com>
2022-02-18 20:48:43 -0300

    feat: criação dos componentes navlink e header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,6 +41,12 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-br">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@200&family=Montserrat:ital,wght@0,300;0,400;0,600;0,700;1,500&display=swap"
+            rel="stylesheet"
+          />
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         </Head>
