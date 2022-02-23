aea8e2738edcf82685b29722e843c79616029ff8
MateusJSouza <mateusjdev@gmail.com>
2022-02-23 13:41:27 -0300

    Criando a sessão de conhecimentos

--- src/components/SectionTitle/index.tsx
+++ src/components/SectionTitle/index.tsx
@@ -1,10 +1,11 @@
+/* eslint-disable react/require-default-props */
 import { ReactNode } from 'react';
 
 import { Container } from './styles';
 
 interface SectionTitleProps {
   title: string | ReactNode;
-  description: string | ReactNode;
+  description?: string | ReactNode;
 }
 
 function SectionTitle({ title, description }: SectionTitleProps) {
